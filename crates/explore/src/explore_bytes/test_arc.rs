789aad48d18637689391fda5a76d6415ae8cdd79
nolmelab <133831637+nolmelab@users.noreply.github.com>
2023-06-02 12:12:58 +0000

    explore Arc<T>

--- crates/explore/src/explore_bytes/test_arc.rs
+++ crates/explore/src/explore_bytes/test_arc.rs
@@ -4,9 +4,12 @@ mod test {
     use std::sync::Arc;
 
     #[test]
-    fn test_arc() {
-        // 아래는 Arc::new에서 Sized가 아니라고 에러를 준다.
-        // Arc<[u8]>을 만들고 싶다. 쩝
-        let av = Arc::from(b"Hello"[..]);
+    fn test_arc_1() {
+        let foo = Arc::new(vec![1, 2, 3]);
+        let foo2 = foo.clone();
+
+        assert_eq!(*foo, *foo2);
+
+        println!("{:?}", *foo);
     }
 }
