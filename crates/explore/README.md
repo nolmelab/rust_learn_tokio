279e841107a28fb6604babf1685eb208538cdde9
nolmelab <133831637+nolmelab@users.noreply.github.com>
2023-06-02 07:26:12 +0000

    explore cargo

---
+++ crates/explore/README.md
@@ -0,0 +1,5 @@
+# explore
+
+rust tutorial을 따라가면서 만나는 라이브러리를 익힙니다. 
+먼저 bytes입니다. 
+
---
+++ crates/explore/src/explore_bytes/test.rs
@@ -0,0 +1,9 @@
+use bytes::{Bytes, BytesMut, Buf, BufMut};
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn test_bytes_usage() {
+
+    }
+}
